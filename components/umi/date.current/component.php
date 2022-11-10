079de762b4ce0ae1fe41a7ebe894fcc6f7f58e5e
umi777 <57103886+umi777@users.noreply.github.com>
2022-11-10 15:37:00 +0700

    Рефакторинг кода

--- components/umi/date.current/component.php
+++ components/umi/date.current/component.php
@@ -1,9 +1,5 @@
 <? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 $arResult['DATE'] = date($arParams["TEMPLATE_FOR_DATE"]);
-if (getdate()['wday'] == 0 || getdate()['wday'] == 6) {
-    $arResult['weekend'] = true;
-} else {
-    $arResult['weekend'] = false;
-}
+$arResult['weekend'] = (getdate()['wday'] == 0 || getdate()['wday'] == 6);
 $this->IncludeComponentTemplate();
 ?>
