fb108faa6ac86ee0b90fd297c9e22addd94a201b
umi777 <57103886+umi777@users.noreply.github.com>
2022-11-10 15:00:10 +0700

    Компонент с входными параметрами

--- components/umi/date.current/component.php
+++ components/umi/date.current/component.php
@@ -1,5 +1,4 @@
 <? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-
-$arResult['DATE'] = date('Y-m-d');
+$arResult['DATE'] = date($arParams["TEMPLATE_FOR_DATE"]);
 $this->IncludeComponentTemplate();
 ?>
