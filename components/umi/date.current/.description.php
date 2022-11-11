8dd9a31d9ec06e6f9c816a653c4c5c14780579b0
Максим Иванович <umi-9956@yandex.ru>
2022-11-11 13:59:15 +0700

    update .description.php

--- components/umi/date.current/.description.php
+++ components/umi/date.current/.description.php
@@ -1,6 +1,6 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die(); 
 $arComponentDescription = array(
-    "NAME" => GetMessage("Текущая дата"),
+    "NAME" => "Текущая дата",
     "DESCRIPTION" => GetMessage("Выводим текущую дату"),
     "PATH" => array(
         "ID" => "umi_components",
