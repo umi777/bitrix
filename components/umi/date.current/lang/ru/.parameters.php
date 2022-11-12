ae23f8b4579a8807620759289c7c536b6b5b2a4c
Максим Иванович <umi-9956@yandex.ru>
2022-11-12 16:28:47 +0700

    Исправление ошибки

--- components/umi/date.current/lang/ru/.parameters.php
+++ components/umi/date.current/lang/ru/.parameters.php
@@ -3,5 +3,4 @@ $MESS["IBLOCK_CODE"] = "Символьный код инфоблока";
 $MESS["TEMPLATE_FOR_DATE"] = "Шаблон для даты";
 $MESS["UMI_DT_IBLOCK_TYPE"] = "Тип инфоблока";
 $MESS["UMI_DT_IBLOCK"] = "Инфоблок";
-$
 ?>
