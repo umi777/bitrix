c260197a99ac0a0e5460b73242bb60ea27724f4b
Максим Иванович <umi-9956@yandex.ru>
2022-11-12 13:43:28 +0700

    Добавлены выборы типа инфоблока и самого инфоблока

--- components/umi/date.current/.parameters.php
+++ components/umi/date.current/.parameters.php
@@ -1,4 +1,18 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+if(!CModule::IncludeModule("iblock"))
+	return;
+
+$arIBlockType = CIBlockParameters::GetIBlockTypes();
+
+$arIBlock=array();
+$rsIBlock = CIBlock::GetList(Array("SORT" => "ASC"), Array("TYPE" => $arCurrentValues["IBLOCK_TYPE"], "ACTIVE"=>"Y"));
+while($arr=$rsIBlock->Fetch())
+{
+	$arIBlock[$arr["ID"]] = "[".$arr["ID"]."] ".$arr["NAME"];
+}
+
+
  $arComponentParameters = array(
         "GROUPS" => array(),
         "PARAMETERS" => array(
@@ -15,6 +29,21 @@
                 "TYPE" => "STRING",
                 "MULTIPLE" => "N",
                 "DEFAULT" => "weekend",
+            ),
+            "IBLOCK_TYPE" => array(
+            	"PARENT" => "BASE",
+            	"NAME" => GetMessage("UMI_DT_IBLOCK_TYPE"),
+            	"TYPE" => "LIST",
+	            "VALUES" => $arIBlockType,
+	            "REFRESH" => "Y",
+            ),
+            "IBLOCK_ID" => array(
+            	"PARENT" => "BASE",
+            	"NAME" => GetMessage("UMI_DT_IBLOCK"),
+	            "TYPE" => "LIST",
+            	"VALUES" => $arIBlock,
+            	"REFRESH" => "Y",
+	            "ADDITIONAL_VALUES" => "Y",
             ),
         ),
     );
